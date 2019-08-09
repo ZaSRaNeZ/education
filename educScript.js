5c63d6770e4b23d47946a7b24d2cd2c852168b82
ZaSRaNeZ <53185125+ZaSRaNeZ@users.noreply.github.com>
2019-08-09 14:02:26 +0300

    Search only on head

--- educScript.js
+++ educScript.js
@@ -1,4 +1,4 @@
-var menuButtons = $('a.fl-l');
+var menuButtons = $('.menu.fl-l').find('a.fl-l');
 var ElIndex = 0;
 
 $('<style>#modalEducation {position: absolute;z-index: 1000;width: 300px;min-height: 150px;background: #fff;border-radius: 5px;box-shadow: 0 0 10px #d0d6ff; transition: all 0.4s ease 0s;}#EducationButtons{position: absolute;bottom: -35px;width: 100%;height: 30px;display: flex;justify-content: space-between;}div#endEducation, div#next {height: 30px;line-height: 22px;width: 50%;text-align: center;padding: 4px;box-sizing: border-box;color: #fff; cursor: pointer;}div#endEducation{border-radius: 5px 0 0 5px;border: 1px solid #f78a8a91;color: #f78a8a;border-right: none;}div#next{border: 1px solid #8bc34a85;border-radius: 0 5px 5px 0;color: #8bc34a;border-left:none;}div#endEducation:hover{background: #f78a8a;color: #fff;}div#next:hover{background:#8bc34a;color:#fff;}div#modalEducationText{margin: 12px;}</style>').appendTo('body');
