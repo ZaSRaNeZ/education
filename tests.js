494428b2c7a687e2b5628de097f941f70c971467
ZaSRaNeZ <53185125+ZaSRaNeZ@users.noreply.github.com>
2019-08-21 17:10:55 +0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -3,6 +3,7 @@
 var menuButtons = $('.menu.fl-l').find('a.fl-l');
 var ElIndex = 0;
 
+function educationScriptStart(){
 $('<style>#modalEducation {display:none;position: absolute;z-index: 1000;min-height: 150px;background: #fff;border-radius: 5px;box-shadow: 0 0 10px #d0d6ff; transition: all 0.4s ease 0s; min-width: 300px; max-width: 600px;}#EducationButtons{position: absolute;bottom: -35px;width: 100%;height: 30px;display: flex;justify-content: space-between;}div#endEducation, div#next {height: 30px;line-height: 22px;width: 50%;text-align: center;padding: 4px;box-sizing: border-box;color: #fff; cursor: pointer;}div#endEducation{border-radius: 5px 0 0 5px;border: 1px solid #f78a8a91;color: #f78a8a;border-right: none;}div#next{border: 1px solid #8bc34a85;border-radius: 0 5px 5px 0;color: #8bc34a;border-left:none;}div#endEducation:hover{background: #f78a8a;color: #fff;}div#next:hover{background:#8bc34a;color:#fff;}div#modalEducationText{margin: 12px;} div#modalEducationText img {max-width: 100%;margin-top: 40px;} div#modalEducation_start {position: absolute;top: calc(50% - 200px);left: calc(50% - 300px);z-index: 1000;min-height: 150px;background: #fff;border-radius: 5px;box-shadow: 0 0 10px #6ec4ff;width: 600px;height: 400px;}.startButtons {width: 100%;padding: 10px;box-sizing: border-box;display: flex;justify-content: space-around;}.start_button {width: 45%;height: 80px;line-height: 80px;text-align: center;border: 2px solid #1a8ad8;border-radius: 5px;cursor: pointer;user-select: none;font-size: 24px;color: #1a8ad8; transition: 0.2s all;}.startText{text-align: center;padding:40px; font-size: 1.2rem;}.startText>p{color: #1d1d1d;}.startText>h2{color: #1a8ad8;} .start_button:hover {color: #fff;background: #1a8ad8;}</style>').appendTo('body');
 
 $('<div id="modalEducation"><div id="modalEducationText"></div><div id="EducationButtons"><div id="endEducation">Завершить</div><div id="next">Продолжить</div></div></div>').appendTo('body');
@@ -94,6 +95,8 @@ $('div#modal-overlay').css({
     'background': 'no-repeat center center url(https://i.pinimg.com/originals/9b/53/d4/9b53d4eaf1ffb58eca857738ea925fbb.gif) rgba(0, 0, 0, 0.7)',
 })
 
+
+
 function StartEducation() {
 
     let LoadTimer = setInterval(function() {
@@ -153,3 +156,18 @@ $('#endEducation').click(function() {
     window.location.reload();
 
 });
+};
+
+$(window).load(function(){
+
+$('.menu.fl-l').find('a.fl-l').each(function(ind,el){
+
+if ($(this)[0].innerText == 'Обучалка') {
+    $(this).attr('onClick','educationScriptStart()');
+}
+
+})
+
+})
+
+
